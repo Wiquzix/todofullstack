ea8c2b5f5bb3c6cfd7fd98601c748479ed7b147e
Wiquzix <ctac.nep@gmail.com>
2025-01-25 12:11:42 +0700

    Update db_api.py

--- dbService/db_api.py
+++ dbService/db_api.py
@@ -30,7 +30,7 @@ def get_user(user_id: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return schemas.UserResponse(id=user.id, username=user.username)
 
-@router.get("/users/{username}", response_model=schemas.UserResponse)
+@router.get("/users_by_username/{username}", response_model=schemas.UserResponse)
 def get_user(username: str, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.username == username).first()
     if user is None:
